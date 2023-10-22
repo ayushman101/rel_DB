19167922b90693d0349bfa5c10c26c29479987dd
Ayushman Singh <amansinghayush@gmail.com>
2023-10-22 21:32:40 +0530

    btree updated

--- BTree/btree.go
+++ BTree/btree.go
@@ -1,4 +1,27 @@
 package BTree
 
+type BNode struct {
+	data []byte //can be dumped on the disk easily.
+}
+
+
+const (
+	BNODE_NODE=1
+	BNODE_LEAF=2
+)
+
+type BTree struct {
+	//pointer( not an in-memory pointer)
+	//a 64 bit integer referencing a page on the disk
+	root uint64
+
+	//callbacks for managing on disk pages
+
+	get func(uint64) BNode  // dereference a pointer
+	
+	new func(BNode) uint64 // allocate a new page
+
+	del func(uint64)    // deallocate a page
+}
 
 
