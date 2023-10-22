835853df0a3ae54a6a7b0ff01418c592861a0924
Ayushman Singh <amansinghayush@gmail.com>
2023-10-22 22:10:02 +0530

    pointer methods added on BNode

--- BTree/btree.go
+++ BTree/btree.go
@@ -3,6 +3,7 @@ package BTree
 import(
 	"os"
 	"log"
+	"binary"
 )
 
 //  | type | nkeys | pointers | offsets | key-values
@@ -47,8 +48,7 @@ func init(){
 	// 8=> size of each pointer, 2=> size of offset , 4=> klen and vlen
 	node1max:= HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
 
-	if(node1max> BTREE_PAGE_SIZE)
-		{
+	if node1max> BTREE_PAGE_SIZE{
 			log.Fatal(" node size greater than MAX PAGE SIZE")
 			os.Exit(1)
 		}
@@ -58,14 +58,44 @@ func init(){
 //header of BNode
 
 func (node BNode) btype() uint16{
-	return binary.LittleEndian.uint16(node.data)
+	return binary.LittleEndian.Uint16(node.data)
 }
 
 func (node BNode) nkeys() uint16{
 
-	return binary.LittleEndian.uint16(node.data[2:4])
+	return binary.LittleEndian.Uint16(node.data[2:4])
 }
 
 
+func (node BNode) setHeader(btype uint16, nkeys uint16){
+	binary.LittleEndian.PutUint16(node.data[0:2],btype)
+	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
+}
+
+//pointers
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	if idx >= node.nkeys(){
+		log.Fatal("Index out of bounds")
+		os.Exit(1)
+	}
+
+	pos:= HEADER + 8*idx
+
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	if idx >= node.nkeys() {
+		log.Fatal("Index out of bounds")
+		os.Exit(1)
+	}
+	
+	
+	pos:= HEADER + 8*idx
+
+	binary.LittleEndian.PutUint64(node.data[pos:],val)
+
+}
 
 
