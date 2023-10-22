91fb184bff81301c583e188e81d2b39301b96d91
Ayushman Singh <amansinghayush@gmail.com>
2023-10-22 21:44:21 +0530

    constants added

--- BTree/btree.go
+++ BTree/btree.go
@@ -1,5 +1,15 @@
 package BTree
 
+import(
+	"os"
+	"log"
+)
+
+//  | type | nkeys | pointers | offsets | key-values
+//	2B |   2B  | nkeys*8B | nkeys*2B| ...
+
+// | klen  | vlen  | key  | value
+// | 2B    | 2B    | ...  | ... 
 type BNode struct {
 	data []byte //can be dumped on the disk easily.
 }
@@ -22,6 +32,26 @@ type BTree struct {
 	new func(BNode) uint64 // allocate a new page
 
 	del func(uint64)    // deallocate a page
+}
+
+
+const HEADER=4 // node type: 2B,  nKeys: 2B
+
+const BTREE_PAGE_SIZE=4096
+
+const BTREE_MAX_KEY_SIZE=1000
+
+const BTREE_MAX_VAL_SIZE=3000
+
+func init(){
+	// 8=> size of each pointer, 2=> size of offset , 4=> klen and vlen
+	node1max:= HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+
+	if(node1max> BTREE_PAGE_SIZE)
+		{
+			log.Fatal(" node size greater than MAX PAGE SIZE")
+			os.Exit(1)
+		}
 }
 
 
